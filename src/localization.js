348b7460aed7b3f07307365a321294a20c594b4f
ranbu171 <shulzhenko.dm@gmail.com>
2022-04-25 19:19:36 +0600

    UZS

--- src/localization.js
+++ src/localization.js
@@ -9,11 +9,11 @@ export const texts = {
       less: '',
       enroll: 'Sign up for a course',
       consultation: 'Get Advice',
-      test: 'Pass the test'
+      test: 'Pass the test',
     },
-    homePage : {
-      h1 : 'Learn an in-demand \n<strong class="coloredText">IT- profession </strong>\n and start earning',
-      h2 : {
+    homePage: {
+      h1: 'Learn an in-demand \n<strong class="coloredText">IT- profession </strong>\n and start earning',
+      h2: {
         courses: 'Courses',
         advantages: 'The advantages of studying at Codify',
         internship: 'Guaranteed Internship',
@@ -21,47 +21,46 @@ export const texts = {
         students: 'Employed students',
         studentProjects: 'Works of our students',
         mentors: 'Our Mentors',
-        enroll: 'Get a consultation and sign up for a course'
-
+        enroll: 'Get a consultation and sign up for a course',
       },
       advantages: [
         {
           title: ' Learning from Zero to Junior',
-          body: "Take Codify's authoring program and become a Junior Specialist"
+          body: "Take Codify's authoring program and become a Junior Specialist",
         },
         {
           title: 'Tech Support Team',
-          body: 'Get personalized training from the tech support team'
+          body: 'Get personalized training from the tech support team',
         },
         {
           title: 'Internship at Codify Lab',
-          body: 'Work on real projects in the Codify Lab'
+          body: 'Work on real projects in the Codify Lab',
         },
         {
           title: 'Employment with HR',
-          body: 'Get a course from an HR specialist and get a job in an IT company'
-        }
+          body: 'Get a course from an HR specialist and get a job in an IT company',
+        },
       ],
       mentors: {
         exp: 'Work experience:',
-        company: 'Company:'
+        company: 'Company:',
       },
       internship: {
         employment: 'Employment',
         stages: [
           {
             title: 'Stage 1',
-            body: 'You work\n on internal\n projects from Codify'
+            body: 'You work\n on internal\n projects from Codify',
           },
           {
             title: 'Stage 2',
-            body: 'Working in a team\non real\nprojects'
+            body: 'Working in a team\non real\nprojects',
           },
           {
             title: 'Stage 3',
-            body: 'On successful\ncompletion of the project,\n you get the first\nmoney for the work'
+            body: 'On successful\ncompletion of the project,\n you get the first\nmoney for the work',
           },
-        ]
+        ],
       },
       employment: {
         ourPM: 'Our\nHR-specialist\nwill help:',
@@ -69,29 +68,26 @@ export const texts = {
           'Write a resume and motivation letter',
           'Make a portfolio with projects',
           'Create a Linkedin profile',
-          'Complete a technical interview'
-        ]
-      },   
-        
-
+          'Complete a technical interview',
+        ],
+      },
     },
     footer: {
       about: {
         title: 'About Company',
         academy: 'Adult Courses',
         teens: "Kids' courses",
-        events: 'Events '
+        events: 'Events ',
       },
       projects: {
         title: 'Our Projects ',
         codifyTeens: 'Codify teens',
         lms: 'Codify LMS/CRM',
-        lab: 'Codify LAB'
+        lab: 'Codify LAB',
       },
       contacts: {
         title: 'Contacts',
-        address: "Oltintepa ko'chasi, 28. Tashkent"
-        
+        address: "Oltintepa ko'chasi, 28. Tashkent",
       },
     },
     header: {
@@ -100,7 +96,7 @@ export const texts = {
       events: 'Events',
       contacts: 'Contacts',
       call: 'Connect with us',
-      callUsText: 'Call us or\n write to us (Telegram):\n<b>+998 90 922 44 77</b>\n<b>+998 71 230 77 73</b>'
+      callUsText: 'Call us or\n write to us (Telegram):\n<b>+998 90 922 44 77</b>\n<b>+998 71 230 77 73</b>',
     },
 
     enroll: {
@@ -108,29 +104,29 @@ export const texts = {
       name: 'Name',
       phone: 'Phone number ',
       email: 'E-mail',
-      check: 'By clicking the button, I agree to the processing of personal data and the rules of use of the Platform'
+      check: 'By clicking the button, I agree to the processing of personal data and the rules of use of the Platform',
     },
     courseFirst: {
       format: 'Training format',
       duration: 'Course\nduration:',
       docs: 'Lifetime access\nto materials',
-      internship: 'Guaranteed\nInternship'
+      internship: 'Guaranteed\nInternship',
     },
     coursePlan: {
       h2: 'Course syllabus',
       p1: 'There will be',
-      p2: 'lectures as well as practical assignments based on real cases.'
+      p2: 'lectures as well as practical assignments based on real cases.',
     },
     courseWhy: {
       h2: 'Why',
       h3: 'Cost of training',
-      som: 'KGS/month',
+      som: 'UZS/month',
       payment: '* Payment can be made in installments',
       start: 'Course start:',
       mesto: 'seat',
       mest: 'seats',
       mesta: 'seats',
-      col: 'Remaining:'
+      col: 'Remaining:',
     },
     courseHow: {
       h2: 'The way the class is taught',
@@ -142,8 +138,7 @@ export const texts = {
       p3: 'Every Saturday we hold the IT English Talking Club',
       b4: 'Getting to work',
       p4: ' After the course you get an internship and a job ',
-    }
-    
+    },
   },
 
   ru: {
@@ -156,11 +151,11 @@ export const texts = {
       less: '',
       enroll: 'Записаться на курс',
       consultation: 'Получить консультацию',
-      test: 'Пройти тест'
+      test: 'Пройти тест',
     },
-    homePage : {
-      h1 : 'Освой востребованную\n<strong class="coloredText">IT-профессию</strong>\nи начни зарабатывать',
-      h2 : {
+    homePage: {
+      h1: 'Освой востребованную\n<strong class="coloredText">IT-профессию</strong>\nи начни зарабатывать',
+      h2: {
         courses: 'Курсы',
         advantages: 'Преимущества обучения в Codify',
         internship: 'Гарантированная стажировка',
@@ -168,47 +163,46 @@ export const texts = {
         students: 'Трудоустроенные студенты',
         studentProjects: 'Работы наших студентов',
         mentors: 'Наши менторы',
-        enroll: 'Получить консультацию и записаться на курс'
-
+        enroll: 'Получить консультацию и записаться на курс',
       },
       mentors: {
         exp: 'Опыт работы:',
-        company: 'Место работы:'
+        company: 'Место работы:',
       },
       advantages: [
         {
           title: 'Обучение с нуля до Junior',
-          body: 'Пройди обучение по авторской программе Codify и стань Junior специалистом'
+          body: 'Пройди обучение по авторской программе Codify и стань Junior специалистом',
         },
         {
           title: 'Команда тех.поддержки',
-          body: 'Получи индивидуальные занятия от команды тех.поддержки'
+          body: 'Получи индивидуальные занятия от команды тех.поддержки',
         },
         {
           title: 'Стажировка в Codify Lab',
-          body: 'Работай над реальными проектами в лаборатории Codify'
+          body: 'Работай над реальными проектами в лаборатории Codify',
         },
         {
           title: 'Трудоустройство с HR',
-          body: 'Получи курс от HR специалиста и найди работу в IT компании'
-        }
+          body: 'Получи курс от HR специалиста и найди работу в IT компании',
+        },
       ],
       internship: {
         employment: 'Трудоустройство',
         stages: [
           {
             title: '1 этап',
-            body: 'Вы работаете\nнад внутренними\nпроектами от Codify'
+            body: 'Вы работаете\nнад внутренними\nпроектами от Codify',
           },
           {
             title: '2 этап',
-            body: 'Работаете в команде\nнад реальными\nпроектами'
+            body: 'Работаете в команде\nнад реальными\nпроектами',
           },
           {
             title: '3 этап',
-            body: 'При успешном\nокончании проекта,\n Вы получаете первые\nденьги за работу'
+            body: 'При успешном\nокончании проекта,\n Вы получаете первые\nденьги за работу',
           },
-        ]
+        ],
       },
       employment: {
         ourPM: 'Наш\nHR-специалист\nпоможет:',
@@ -216,29 +210,26 @@ export const texts = {
           'Составить резюме и мотивационное письмо',
           'Оформить портфолио с проектами',
           'Создать Linkedin профиль',
-          'Пройти техническое собеседование'
-        ]
-      },   
-        
-
+          'Пройти техническое собеседование',
+        ],
+      },
     },
     footer: {
       about: {
         title: 'О компании',
         academy: 'Взрослые курсы',
         teens: 'Детские курсы',
-        events: 'Мероприятия'
+        events: 'Мероприятия',
       },
       projects: {
         title: 'Наши проекты',
         codifyTeens: 'Codify teens',
         lms: 'Codify LMS/CRM',
-        lab: 'Codify LAB'
+        lab: 'Codify LAB',
       },
       contacts: {
         title: 'Контакты',
-        address: 'ул. Олтинтепа, 28. Ташкент'
-        
+        address: 'ул. Олтинтепа, 28. Ташкент',
       },
     },
     header: {
@@ -247,36 +238,36 @@ export const texts = {
       events: 'Мероприятия',
       contacts: 'Контакты',
       call: 'Связаться с нами',
-      callUsText: 'Позвоните нам или\nнапишите нам (Telegram):\n<b>+998 90 922 44 77</b>\n<b>+998 71 230 77 73</b>'
+      callUsText: 'Позвоните нам или\nнапишите нам (Telegram):\n<b>+998 90 922 44 77</b>\n<b>+998 71 230 77 73</b>',
     },
     enroll: {
       postMessage: 'Заявка успешно отправлена, с вами свяжутся в ближайшее время',
       name: 'Имя',
       phone: 'Номер телефона',
       email: 'E-mail',
-      check: 'Нажимая на кнопку, я соглашаюсь на обработку персональных данных и с правилами пользования Платформы'
+      check: 'Нажимая на кнопку, я соглашаюсь на обработку персональных данных и с правилами пользования Платформы',
     },
     courseFirst: {
       format: 'формат обучения',
       duration: 'Длительность\nкурса:',
       docs: 'Пожизненный доступ\nк материалам',
-      internship: 'Гарантированная\nстажировка'
+      internship: 'Гарантированная\nстажировка',
     },
     coursePlan: {
       h2: 'Программа курса',
       p1: 'Вас ждут',
-      p2: 'лекции, а также практические задания на основе реальных кейсов.'
+      p2: 'лекции, а также практические задания на основе реальных кейсов.',
     },
     courseWhy: {
       h2: 'Почему',
       h3: 'Стоимость обучения',
-      som: 'сом/месяц',
+      som: 'сум/месяц',
       payment: '*Оплату можно вносить частями',
       start: 'Старт курса:',
       mesto: 'место',
       mest: 'мест',
       mesta: 'места',
-      col: 'Осталось:'
+      col: 'Осталось:',
     },
     courseHow: {
       h2: 'Как проходит обучение',
@@ -288,7 +279,6 @@ export const texts = {
       p3: 'Каждую субботу мы проводим IT English Talking Club',
       b4: 'Начинаете работать',
       p4: 'После курса Вы получаете стажировку и трудоустройство',
-    }
-    
-  }
+    },
+  },
 }
