9a4f5b310073058f592b0a93530467d79f42d4ca
Alisher <zumanovaliser814@gmail.com>
2024-08-08 14:05:00 +0600

    change filter in kids courses and change address second office

--- src/modules/teens-modules/TeensCardsOfCourses.svelte
+++ src/modules/teens-modules/TeensCardsOfCourses.svelte
@@ -1,8 +1,8 @@
 <script>
   import { texts } from '../../localization'
   import { language } from '../../stores'
-  import {request} from "../../api";
-  import TeensCardForCourses from "../../components/ui/Cards/TeensCardForCourses.svelte";
+  import { request } from '../../api'
+  import TeensCardForCourses from '../../components/ui/Cards/TeensCardForCourses.svelte'
 
   let length = 4
   let activeBtn = 0
@@ -12,24 +12,22 @@
 
   language.subscribe(async lang => {
     activeLang = lang
-    request('get', 'children-courses/')
-      .then((data) => {
-        currentCards = data || []
-        filter(0)
-        // if (length && data.length > length) {
-        //   currentCards.length = length
-        // }
-      })
+    request('get', 'children-courses/').then(data => {
+      currentCards = data || []
+      filter(0)
+      // if (length && data.length > length) {
+      //   currentCards.length = length
+      // }
+    })
   })
 
   function filterCards(age_category) {
-    request('get', 'children-courses/', {age_category})
-      .then((data) => {
-        currentCards = data || []
-        // if (length && data.length > length) {
-        //   currentCards.length = length
-        // }
-      })
+    request('get', 'children-courses/', { age_category }).then(data => {
+      currentCards = data || []
+      // if (length && data.length > length) {
+      //   currentCards.length = length
+      // }
+    })
   }
   function filter(tab) {
     activeBtn = tab
@@ -43,133 +41,128 @@
       filterCards('third')
     }
   }
-
 </script>
 
 <section class="sectionCourses">
-    <h2 class="container">{texts[activeLang].homePage.h2.courses}</h2>
-    <div class="tabs-box container">
-
+  <h2 class="container">{texts[activeLang].homePage.h2.courses}</h2>
+  <div class="tabs-box container">
     <div class="sectionCourses__buttons">
-        <button class={`buttonCourse allCourses ${!activeBtn ? 'active0' : ''}`} on:click={() => filter(0)}
+      <button class={`buttonCourse allCourses ${!activeBtn ? 'active0' : ''}`} on:click={() => filter(0)}
         >Все курсы</button
-        >
-        <button class={`buttonCourse dev ${activeBtn === 1 ? 'active1' : ''}`} on:click={() => filter(1)}
-        >Для детей 7-9 лет</button
-        >
-        <button class={`buttonCourse design ${activeBtn === 2 ? 'active2' : ''}`} on:click={() => filter(2)}
-        >Для подростков 10-13 лет</button
-        >
-        <button class={`buttonCourse management ${activeBtn === 3 ? 'active3' : ''}`} on:click={() => filter(3)}
-        >Для подростков 14-17 лет</button
-        >
+      >
+      <button class={`buttonCourse dev ${activeBtn === 1 ? 'active1' : ''}`} on:click={() => filter(1)}
+        >Для детей 7-10 лет</button
+      >
+      <button class={`buttonCourse design ${activeBtn === 2 ? 'active2' : ''}`} on:click={() => filter(2)}
+        >Для подростков 11-14 лет</button
+      >
+      <button class={`buttonCourse management ${activeBtn === 3 ? 'active3' : ''}`} on:click={() => filter(3)}
+        >Для подростков 15-17 лет</button
+      >
     </div>
-    </div>
+  </div>
 
-    <div class="teensCoursesCards container">
-        {#each currentCards as card}
-            <TeensCardForCourses cardInfo={card} />
-        {/each}
-    </div>
+  <div class="teensCoursesCards container">
+    {#each currentCards as card}
+      <TeensCardForCourses cardInfo={card} />
+    {/each}
+  </div>
 </section>
 
 <style>
-    .sectionCourses {
-        text-align: center;
-    }
+  .sectionCourses {
+    text-align: center;
+  }
+  .tabs-box {
+    display: flex;
+    justify-content: flex-start;
+    overflow: hidden;
+  }
+  .sectionCourses__buttons {
+    display: flex;
+    overflow-x: auto;
+    gap: 20px;
+    padding-bottom: 20px;
+  }
+  @media (max-width: 768px) {
     .tabs-box {
-        display: flex;
-        justify-content: flex-start;
-        overflow: hidden;
+      width: 100%;
     }
     .sectionCourses__buttons {
-        display: flex;
-        overflow-x: auto;
-        gap: 20px;
-        padding-bottom: 20px;
+      padding: 0 5vw 20px;
+      gap: 5px;
     }
-    @media (max-width: 768px) {
-        .tabs-box {
-            width: 100%;
-        }
-        .sectionCourses__buttons {
-            padding: 0 5vw 20px;
-            gap: 5px
-        }
-    }
+  }
+  .teensCoursesCards {
+    margin-top: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    gap: 20px;
+    flex-wrap: wrap;
+  }
+  @media (max-width: 1000px) {
     .teensCoursesCards {
-        margin-top: 0;
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        justify-content: center;
-        gap: 20px;
-        flex-wrap: wrap;
+      grid-template-columns: 1fr;
     }
-    @media (max-width: 1000px) {
-        .teensCoursesCards {
-            grid-template-columns: 1fr;
-        }
-    }
-    .buttonCourse {
-        border-radius: 10px;
-        border: 1px solid;
-        background-color: #131315;
-        padding: 10px 30px;
-        transition: 0.2s all;
-        white-space: nowrap;
-        flex-shrink: 0;
-    }
-    .allCourses {
-        border-color: rgba(247, 148, 30, 1);
-        /*filter: drop-shadow(4px 4px 10px rgba(247, 149, 30, 0.13));*/
-        transform: translate3d(0, 0, 0);
-    }
+  }
+  .buttonCourse {
+    border-radius: 10px;
+    border: 1px solid;
+    background-color: #131315;
+    padding: 10px 30px;
+    transition: 0.2s all;
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+  .allCourses {
+    border-color: rgba(247, 148, 30, 1);
+    /*filter: drop-shadow(4px 4px 10px rgba(247, 149, 30, 0.13));*/
+    transform: translate3d(0, 0, 0);
+  }
 
-    .dev {
-        border-color: rgba(103, 3, 180, 1);
-        /*filter: drop-shadow(4px 4px 10px rgba(169, 0, 188, 0.199));*/
-        transform: translate3d(0, 0, 0);
+  .dev {
+    border-color: rgba(103, 3, 180, 1);
+    /*filter: drop-shadow(4px 4px 10px rgba(169, 0, 188, 0.199));*/
+    transform: translate3d(0, 0, 0);
+  }
+  .design {
+    border-color: #037fb4;
+    /*filter: drop-shadow(4px 4px 10px rgba(3, 127, 180, 0.7));*/
 
-    }
-    .design {
-        border-color: #037FB4;
-        /*filter: drop-shadow(4px 4px 10px rgba(3, 127, 180, 0.7));*/
+    transform: translate3d(0, 0, 0);
+  }
+  .management {
+    border-color: rgba(221, 255, 0, 1);
+    /*filter: drop-shadow(4px 4px 10px #ddff002d);*/
+    transform: translate3d(0, 0, 0);
+  }
+  .buttonCourse:hover {
+    border-color: var(--light-blue) !important;
+  }
+  .buttonCourse:active {
+    transform: scale(0.99);
+    border-color: var(--green) !important;
+    filter: none !important;
+  }
+  .sectionCourses .button {
+    margin-top: 40px;
+  }
+  .active0 {
+    background: rgba(247, 148, 30, 1);
+    color: black;
+  }
+  .active1 {
+    background: rgba(103, 3, 180, 1);
+  }
+  .active2 {
+    background: #037fb4;
 
-        transform: translate3d(0, 0, 0);
+    color: black;
+  }
+  .active3 {
+    background: rgba(221, 255, 0, 1);
 
-    }
-    .management {
-        border-color: rgba(221, 255, 0, 1);
-        /*filter: drop-shadow(4px 4px 10px #ddff002d);*/
-        transform: translate3d(0, 0, 0);
-
-    }
-    .buttonCourse:hover {
-        border-color: var(--light-blue) !important;
-    }
-    .buttonCourse:active {
-        transform: scale(0.99);
-        border-color: var(--green) !important;
-        filter: none !important;
-    }
-    .sectionCourses .button {
-        margin-top: 40px;
-    }
-    .active0 {
-        background: rgba(247, 148, 30, 1);
-        color: black;
-    }
-    .active1 {
-        background: rgba(103, 3, 180, 1);
-    }
-    .active2 {
-        background:#037FB4;
-
-        color: black;
-    }
-    .active3 {
-        background: rgba(221, 255, 0, 1);
-
-        color: black;
-    }
+    color: black;
+  }
 </style>
--- src/modules/teens-modules/TeensLocation.svelte
+++ src/modules/teens-modules/TeensLocation.svelte
@@ -1,134 +1,115 @@
 <script>
-
 </script>
 
 <section class="location-section container">
-    <h2>
-        Филиалы нашей Академии
-    </h2>
-    <div class="location-container">
-        <div class="location-block">
-            <img class="location-image" src="./assets/images/place-1.png" alt="location">
-            <h5 class="title">
-                CODIFY Академия в 7 микрорайоне
-            </h5>
-            <div class="location-info_box">
-                <img src="./assets/icons/new-phone.svg" alt="phone">
-                <p>
-                    +996 500 431 430
-                </p>
-            </div>
-            <div class="location-info_box">
-                <img src="./assets/icons/new-location.svg" alt="location">
-                <p>
-                    г. Бишкек, 7 микрорайон, 23
-                </p>
-            </div>
-            <a href="https://2gis.kg/bishkek/branches/70000001038163167/firm/70000001063954838?m=74.617117%2C42.828746%2F17.62" target="_blank" >
-
-            <button class="button contained">
-                Смотреть на карте
-            </button>
-            </a>
-
-        </div>
-        <div class="location-block">
-            <img class="location-image" src="./assets/images/place-2.png" alt="location">
-            <h5 class="title">
-                CODIFY Академия на Исанова
-            </h5>
-            <div class="location-info_box">
-                <img src="./assets/icons/new-phone.svg" alt="phone">
-                <p>
-                    +996 500 431 430
-                </p>
-            </div>
-            <div class="location-info_box">
-                <img src="./assets/icons/new-location.svg" alt="location">
-                <p>
-                    г. Бишкек, Исанова 105/3, БЦ Destination
-                </p>
-            </div>
-            <a href="https://2gis.kg/bishkek/branches/70000001038163167/firm/70000001038163168?m=74.592833%2C42.877367%2F18.31" target="_blank" >
-
-                <button class="button contained">
-                    Смотреть на карте
-                </button>
-            </a>
-        </div>
+  <h2>Филиалы нашей Академии</h2>
+  <div class="location-container">
+    <div class="location-block">
+      <img class="location-image" src="./assets/images/place-1.png" alt="location" />
+      <h5 class="title">CODIFY Академия в 7 микрорайоне</h5>
+      <div class="location-info_box">
+        <img src="./assets/icons/new-phone.svg" alt="phone" />
+        <p>+996 500 431 430</p>
+      </div>
+      <div class="location-info_box">
+        <img src="./assets/icons/new-location.svg" alt="location" />
+        <p>г. Бишкек, 7 микрорайон, 23</p>
+      </div>
+      <a
+        href="https://2gis.kg/bishkek/branches/70000001038163167/firm/70000001063954838?m=74.617117%2C42.828746%2F17.62"
+        target="_blank"
+      >
+        <button class="button contained"> Смотреть на карте </button>
+      </a>
     </div>
+    <div class="location-block">
+      <img class="location-image" src="./assets/images/place-2.png" alt="location" />
+      <h5 class="title">CODIFY Академия на Исанова</h5>
+      <div class="location-info_box">
+        <img src="./assets/icons/new-phone.svg" alt="phone" />
+        <p>+996 500 431 430</p>
+      </div>
+      <div class="location-info_box">
+        <img src="./assets/icons/new-location.svg" alt="location" />
+        <p>г. Бишкек, Ибраимова 115, БЦ Асыл-Таш</p>
+      </div>
+      <a href="https://go.2gis.com/u44oz" target="_blank">
+        <button class="button contained"> Смотреть на карте </button>
+      </a>
+    </div>
+  </div>
 </section>
 
 <style>
+  .location-container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    width: 100%;
+    gap: 20px;
+  }
+  .location-block {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  .location-block .location-image {
+    width: 100%;
+    object-fit: cover;
+    aspect-ratio: 2 / 1;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .location-block .title {
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+  .location-block .location-info_box {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  .location-block .location-info_box p {
+    color: #009dff;
+    font-size: 20px;
+    font-style: normal;
+    line-height: normal;
+  }
+  .location-block a {
+    margin-top: auto;
+  }
+  .location-block a button {
+    margin-top: 10px;
+  }
+
+  @media (max-width: 768px) {
     .location-container {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        width: 100%;
-        gap: 20px;
+      grid-template-columns: 100%;
     }
     .location-block {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
+      gap: 5px;
     }
-    .location-block .location-image {
-        width: 100%;
-        object-fit: cover;
-        aspect-ratio: 2 / 1;
-        margin-bottom: 10px;
-        border-radius: 10px;
-        overflow: hidden;
-    }
+
     .location-block .title {
-        font-size: 24px;
-        font-style: normal;
-        font-weight: 500;
-        line-height: normal;
+      font-size: 20px;
     }
-    .location-block .location-info_box {
-        display: flex;
-        align-items: center;
-        gap: 8px;
-    }
+
     .location-block .location-info_box p {
-        color: #009DFF;
-        font-size: 20px;
-        font-style: normal;
-        line-height: normal;
+      font-size: 14px;
+    }
+    .location-block .location-info_box img {
+      width: 20px;
+      height: 20px;
     }
     .location-block a {
-        margin-top: auto;
+      display: none;
+      width: 100%;
+      margin-bottom: 10px;
     }
     .location-block a button {
-        margin-top: 10px;
+      width: 100% !important;
     }
-
-    @media (max-width: 768px) {
-        .location-container {
-            grid-template-columns: 100%;
-        }
-        .location-block {
-            gap: 5px;
-        }
-
-        .location-block .title {
-            font-size: 20px;
-        }
-
-        .location-block .location-info_box p {
-            font-size: 14px;
-        }
-        .location-block .location-info_box img {
-            width: 20px;
-            height: 20px;
-        }
-        .location-block a {
-            display: none;
-            width: 100%;
-            margin-bottom: 10px;
-        }
-        .location-block a button {
-            width: 100% !important;
-        }
-    }
+  }
 </style>
